b77f07f9e895095f24b76930b53f83f707b1c599
Kevin Guerrero <guerrk83z@gmail.com>
2021-12-01 09:15:16 -0500

    commit tree 3b66422afd379c464db5a12ee1507d522bba1451

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+function submitData (userName, userEmail) {
+    const data = {
+        name : userName,
+        email : userEmail
+    }
+
+    const configurationObject = {
+        method : "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            Accept : "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+    
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((json) => document.body.append(json.id))
+    .catch(error => document.body.append(error.message))
+
+}
